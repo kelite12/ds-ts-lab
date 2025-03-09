a06830ba08153610c81fb1d80423dc1db336d14a
kelite12 <20108870@mail.wit.ie>
2025-03-09 18:31:32 +0000

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,2 +1,8 @@
 import { Friend, Colleague } from './myTypes';
 import { friends, colleagues } from "./01-basics";
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
