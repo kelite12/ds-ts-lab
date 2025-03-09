044a264f6d302d18f48feb2568356bd75279acb5
kelite12 <20108870@mail.wit.ie>
2025-03-09 18:24:01 +0000

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,27 +1,37 @@
-// (Optional) Change the names below to your friends.
 interface Friend {
     name: string;
     phone: string;
-    age: number
-}
-
-const friend1 = {
+    age: number;
+  }
+  
+  const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
   };
   
-  const friend2 = {
+  const friend2: Friend = {
     name: "Jane Costello",
     phone: "086--12345",
     age: 31,
   };
   
-  const friends = [friend1, friend2];
+  const friends: Friend[] = [friend1, friend2];
   console.log(friends[1]);
   
-  //   -------------------
-  const colleague1 = {
+  // -------------------
+  interface Contact {
+    email: string;
+    extension: number;
+  }
+  
+  interface Colleague {
+    name: string;
+    department: string;
+    contact: Contact;
+  }
+  
+  const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -30,7 +40,7 @@ const friend1 = {
     },
   };
   
-  const colleague2 = {
+  const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -39,7 +49,7 @@ const friend1 = {
     },
   };
   
-  const colleague3 = {
+  const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
@@ -47,9 +57,16 @@ const friend1 = {
       extension: 125,
     },
   };
-  const colleagues = {
+  
+  interface ColleagueHistory {
+    current: Colleague[];
+    former: Colleague[];
+  }
+  
+  export const colleagues: ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
   
   console.log(colleagues.current[0]);
+  
